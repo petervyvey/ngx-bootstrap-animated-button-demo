788cf4da95ffd12ede811d164588ff764caec910
Peter Vyvey <peter.vyvey@gmail.com>
2018-05-04 12:19:12 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/app/shared/containers/layout/layout.module.ts
+++ src/app/shared/containers/layout/layout.module.ts
@@ -1,7 +1,15 @@
+<<<<<<< Updated upstream:src/app/shared/containers/layout/layout.module.ts
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NavigationModule } from '@containers/navigation/navigation.module';
 import { LayoutComponent } from './layout.component';
+=======
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {NavigationComponent} from '@components/navigation/navigation.component';
+import {NavigationModule} from '@components/navigation/navigation.module';
+import {LayoutComponent} from './layout.component';
+>>>>>>> Stashed changes:src/app/shared/components/layout/layout.module.ts
 
 @NgModule({
     imports: [
