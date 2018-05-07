635a8110a92fa4d89ccb9620c275935f25a7adf5
Peter Vyvey <peter.vyvey@gmail.com>
2018-05-07 06:16:18 +0200

    transcluded content

--- src/app/shared/components/animated-button/animated-button.component.ts
+++ src/app/shared/components/animated-button/animated-button.component.ts
@@ -11,22 +11,35 @@ export class AnimatedButtonComponent implements OnInit {
 
     constructor() { }
 
+    private static readonly DEFAULT_SUBMITTING_TEXT: string = 'Submitting...';
+
     buttonCssClasses: ICssClasses = {};
     contentCssClasses: ICssClasses = {};
 
     private options$ = new BehaviorSubject<Partial<IAnimatedButtonSettings>>({});
     private settings$ = new BehaviorSubject<IAnimatedButtonSettings>(Object.assign({}, animatedButtonDefaultSettings));
+    public animating$ = new BehaviorSubject<boolean>(true);
+    public animatingText$ = new BehaviorSubject<string>(AnimatedButtonComponent.DEFAULT_SUBMITTING_TEXT);
 
     @Input()
-    get options(): Partial<IAnimatedButtonSettings> { return this.options$.value; }
     set options(value: Partial<IAnimatedButtonSettings>) { this.options$.next(value); }
+
+    @Input()
+    set animating(value: boolean) { this.animating$.next(value); }
+
+    @Input()
+    set animatingText(value: string) { this.animatingText$.next(value); }
+
     get settings(): IAnimatedButtonSettings { return this.settings$.value; }
     set settings(value: IAnimatedButtonSettings) { this.settings$.next(value); }
+
     ngOnInit() {
         this.options$
             .map(options => this.settings = {...animatedButtonDefaultSettings, ...options})
             .subscribe(
                 settings => {
+                    this.buttonCssClasses['sa-btn-animated'] = true;
+
                     this.buttonCssClasses['btn'] = true;
                     this.buttonCssClasses['btn-primary'] = settings.defaultClass === 'btn-primary';
                     this.buttonCssClasses['btn-warning'] = settings.defaultClass === 'btn-warning';
